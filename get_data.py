034dcbfcd7fb50b7204987f5d6ebbf1ffdd47323
Darshil Shah <dks53@georgetown.edu>
2020-02-12 18:16:10 -0500

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -1,1 +1,25 @@
+# get_Data.py
+
+import requests
+import json
+
 print("REQUESTING SOME DATA FROM THE INTERNET...")
+
+request_url = "https://raw.githubusercontent.com/prof-rossetti/intro-to-python/master/data/products.json"
+print("URL: ", request_url)
+
+response = requests.get(request_url)
+print(type(response))
+#print(dir(response))
+print(response.status_code)
+#print(response.text)
+print(type(response.text))
+
+parsed_response = json.loads(response.text)
+print(type(parsed_response)) # > dictionary
+#print(parsed_response["name"])
+
+breakpoint()
+
+#for d in parsed_response:
+#    print(d["id"], d["name"])
